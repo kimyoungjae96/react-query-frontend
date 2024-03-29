08dd4a9d5241e19d4c677ed48ec0efd2c726ab72
96kimyoungjae <sskim5421@gmail.com>
2024-03-29 15:39:53 +0900

    feat: ErrorBoundary 추가

--- src/pages/posts/[id]/index.tsx
+++ src/pages/posts/[id]/index.tsx
@@ -1,14 +1,36 @@
 import { Header, PostDetail } from "@/components";
+import { useQueryErrorResetBoundary } from "@tanstack/react-query";
 
 import { Suspense } from "react";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+
+function fallbackRender({ error, resetErrorBoundary }: FallbackProps) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+      <button
+        onClick={() => {
+          resetErrorBoundary();
+        }}
+      >
+        reset
+      </button>
+    </div>
+  );
+}
 
 export const PostDetailPage = () => {
+  const { reset } = useQueryErrorResetBoundary();
+
   return (
     <div>
       <Header />
-      <Suspense fallback="로딩중...">
-        <PostDetail />
-      </Suspense>
+      <ErrorBoundary onReset={reset} fallbackRender={fallbackRender}>
+        <Suspense fallback="로딩중...">
+          <PostDetail />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
